1a702eaa9e77a86527abfea3854752dbf2a57ced
iso166 <gozde3373@hotmail.com>
2026-06-16 10:46:29 +0300

    farklıbirsey

--- main.py
+++ main.py
@@ -1,248 +1,198 @@
 import tkinter as tk
-import mysql.connector
-import bcrypt
 import anasayfa
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  
-
-db_user = os.getenv("DB_USER")      
-db_password = os.getenv("DB_PASSWORD")  
+import requests
 
 print("hello")
 
+url = "http://127.0.0.1:8000/"
 
-class LoginApp:
 
-    def __init__(self):
-        self.x = 0
-        self.db_config = {
-            'host': 'localhost',
-            'user': db_user,
-            'password': db_password,
-            'database': 'testdb'
-        }
 
-        self.conn = mysql.connector.connect(**self.db_config)
+def sorgu(kullanıcı, sifre):
+    print("istek gönderiliyor")
+    data = {
+    "username": kullanıcı,
+    "password": sifre
+    }
+    data["username"] = kullanıcı
+    data["password"] = sifre
 
-        self.cursor = self.conn.cursor()
+    try:
+        cevap = requests.post(url+"login", json=data, timeout=5)
 
-        print("bağlantı tamam")
+        print(cevap.status_code)
+        print(cevap.json())
 
-        self.sorgu_SQL = "SELECT Password FROM Kullanıcılar WHERE Nickname = %s"
+        if cevap.status_code == 200:
+            sonuc = cevap.json()
+            print(sonuc)
 
-        self.pencere = tk.Tk()
-
-        self.pencere.title("Giriş")
-
-        self.pencere.geometry("400x250")
-
-        self.pencere.resizable(False, False)
-
-        self.label_0 = tk.Label(
-            text="Giriş",
-            font=("Arial", 16, "bold")
-        )
-
-        self.label_1 = tk.Label(
-            text="Kullanıcı Adı"
-        )
-
-        self.label_2 = tk.Label(
-            text="Şifre"
-        )
-
-        self.entry_0 = tk.Entry()
-
-        self.entry_1 = tk.Entry(show="*")
-
-        self.button_0 = tk.Button(
-            text="giriş",
-            command=lambda: self.sorgu(
-                self.entry_0.get(),
-                self.entry_1.get()
-            )
-        )
-
-        self.button_1 = tk.Button(
-            text="Kayıt ol",
-            command=self.kayıt_panel
-        )
-
-        self.label_0.pack()
-
-        self.label_1.pack()
-
-        self.entry_0.pack()
-
-        self.label_2.pack()
-
-        self.entry_1.pack()
-
-        self.button_0.pack()
-
-        self.button_1.pack()
-
-        self.pencere.mainloop()
-
-    def sorgu(self, girilen_kullanici, girilen_sifre):
-
-        print("sorgu başladı")
-
-        if not girilen_kullanici or not girilen_sifre:
-            print("boş")
-            return
-
-        self.cursor.execute(self.sorgu_SQL, (girilen_kullanici,))
-
-        sonuc = self.cursor.fetchone()
-
-        if sonuc:
-
-            db_sifre_hash = sonuc[0]
-
-            if isinstance(db_sifre_hash, str):
-                db_sifre_hash = db_sifre_hash.encode('utf-8')
-
-            girilen_sifre_bytes = girilen_sifre.encode('utf-8')
-
-            if bcrypt.checkpw(girilen_sifre_bytes, db_sifre_hash):
-
-                print(f"[BAŞARILI] Giriş yapıldı! Hoş geldiniz: {girilen_kullanici}")
-                self.x = 0
-                self.cursor.close()
-                self.conn.close()
-                self.pencere.destroy()
-                anasayfa.app()
+            if sonuc.get("status") == "success":
+                print("Giriş başarılı")
             else:
+                print("Giriş başarısız")
+        else:
+            print("Server hatası")
 
-                print("[HATA] Hatalı şifre yada kullanıcı adı!")
-                self.hata_kontrol()
-        else:       
+    except requests.exceptions.RequestException as e:
+        print("Bağlantı hatası:", e)
 
-            print("[HATA] Hatalı şifre yada kullanıcı adı!")
-            self.hata_kontrol()
+def kaydet_post(kullanıcı,sifre,tekrar,mail):
+    print("Kayıt")
+    data_2 = {
+    "username": kullanıcı,
+    "password": sifre,
+    "password2": tekrar,
+    "mail": mail
+    }
 
-    def kaydet_veritabani(self, yeni_kullanici, yeni_sifre, alt_pencere):
+    try:
+        durum = requests.post(url+"singup",json=data_2, timeout=5)
+        print(durum.status_code)
+        print(durum.json())
+        if durum.status_code == 200:
+            sonuc = durum.json()
+            print(sonuc)
 
-        print("Kayıt işlemi başladı...")
-
-        KAYIT_SQL = """
-INSERT INTO Kullanıcılar (Nickname, Password)
-VALUES (%s, %s)
-"""
-
-        if not yeni_kullanici or not yeni_sifre:
-
-            print("[HATA] Kayıt için kullanıcı adı veya şifre boş bırakılamaz!")
-
-            return
-
-        try:
-
-            sifre_bytes = yeni_sifre.encode('utf-8')
-
-            tuz = bcrypt.gensalt()
-
-            hashlenmis_sifre = bcrypt.hashpw(sifre_bytes, tuz)
-
-            self.cursor.execute(
-                KAYIT_SQL,
-                (yeni_kullanici, hashlenmis_sifre.decode('utf-8'))
-            )
-
-            self.conn.commit()
-
-            print(f"[BAŞARILI] {yeni_kullanici} başarıyla kayıt edildi!")
-
-            alt_pencere.destroy()
-
-            self.pencere.deiconify()
-
-        except mysql.connector.Error as err:
-            if err.errno == 1062:
-                print("[HATA] Bu kullanıcı adı zaten kullanılıyor!")
+            if sonuc.get("status") == "success":
+                print("Giriş başarılı")
             else:
-                print(f"[HATA] Veritabanı hatası: {err}")
+                print("Giriş başarısız")
+        else:
+            print("Server hatası")
 
-    def hata_kontrol(self):
-        self.x += 1
+    except requests.exceptions.RequestException as e:
+        print("Bağlantı hatası:", e)
 
-        if self.x >= 5:
-            print("[HATA] Çok fazla yanlış giriş!")
-            self.pencere.withdraw()
-            self.pencere.after(10000, self.pencere.deiconify)
-            self.x = 0
+def kayıt_panel():
+    
+    print("Kayıt yapılıyor")
 
-    def kayıt_panel(self):
-        
-        print("Kayıt yapılıyor")
+    pencere.withdraw()
 
-        self.pencere.withdraw()
+    alt_pencere = tk.Toplevel()
 
-        alt_pencere = tk.Toplevel()
+    alt_pencere.title("Kayıt ol")
 
-        alt_pencere.title("Kayıt ol")
+    alt_pencere.geometry("400x250")
 
-        alt_pencere.geometry("400x250")
+    alt_pencere.resizable(False, False)
 
-        alt_pencere.resizable(False, False)
+    def geridon():
 
-        def geridon():
+        print("geri dönüldü")
 
-            print("geri dönüldü")
+        alt_pencere.destroy()
 
-            alt_pencere.destroy()
+        pencere.deiconify()
 
-            self.pencere.deiconify()
+    label_3 = tk.Label(
+        alt_pencere,
+        text="Kayıt ol",
+        font=("Arial", 16, "bold")
+    )
 
-        label_3 = tk.Label(
-            alt_pencere,
-            text="Kayıt ol",
-            font=("Arial", 16, "bold")
+    label_4 = tk.Label(
+        alt_pencere,
+        text="Kullanıcı adı"
+    )
+
+    label_5 = tk.Label(
+        alt_pencere,
+        text="Şifre"
+    )
+
+    label_6 = tk.Label(
+        alt_pencere,
+        text="Şifreyi tekrarla"
+    )    
+    label_7 = tk.Label(
+        alt_pencere,
+        text="Mail"
+    )
+    entry_2 = tk.Entry(alt_pencere)
+
+    entry_3 = tk.Entry(
+        alt_pencere,
+        show="*"
+    )
+    entry_4 = tk.Entry(
+        alt_pencere,
+        show="*"
+    )
+    entry_5 = tk.Entry(alt_pencere)
+
+    button_2 = tk.Button(
+        alt_pencere,
+        text="Kayıt ol",
+        command=lambda: kaydet_post(
+            entry_2.get(),
+            entry_3.get(),
+            entry_4.get(),
+            entry_5.get(),
         )
+    )
 
-        label_4 = tk.Label(
-            alt_pencere,
-            text="Kullanıcı adı"
-        )
+    label_3.pack()
+    label_4.pack()
+    entry_2.pack()
+    label_5.pack()
+    entry_3.pack()
+    label_6.pack()
+    entry_4.pack()
+    label_7.pack()
+    entry_5.pack()
+    button_2.pack()
 
-        label_5 = tk.Label(
-            alt_pencere,
-            text="Şifre"
-        )
-
-        entry_2 = tk.Entry(alt_pencere)
-
-        entry_3 = tk.Entry(
-            alt_pencere,
-            show="*"
-        )
-
-        button_2 = tk.Button(
-            alt_pencere,
-            text="Kayıt ol",
-            command=lambda: self.kaydet_veritabani(
-                entry_2.get(),
-                entry_3.get(),
-                alt_pencere
-            )
-        )
-
-        label_3.pack()
-
-        label_4.pack()
-
-        entry_2.pack()
-
-        label_5.pack()
-
-        entry_3.pack()
-
-        button_2.pack()
-
-        alt_pencere.protocol("WM_DELETE_WINDOW", geridon)
+    alt_pencere.protocol("WM_DELETE_WINDOW", geridon)
 
 
-LoginApp()
+pencere = tk.Tk()
+
+pencere.title("Giriş")
+
+pencere.geometry("400x250")
+
+pencere.resizable(False, False)
+
+label_0 = tk.Label(
+    text="Giriş",
+    font=("Arial", 16, "bold")
+)
+
+label_1 = tk.Label(
+    text="Kullanıcı Adı"
+)
+
+label_2 = tk.Label(
+    text="Şifre"
+)
+
+entry_0 = tk.Entry()
+
+entry_1 = tk.Entry(show="*")
+
+button_0 = tk.Button(
+    text="giriş",
+    command=lambda: sorgu(
+        entry_0.get(),
+        entry_1.get()
+    )
+)
+
+button_1 = tk.Button(
+    text="Kayıt ol",
+    command=kayıt_panel
+)
+
+label_0.pack()
+label_1.pack()
+entry_0.pack()
+label_2.pack()
+entry_1.pack()
+button_0.pack()
+button_1.pack()
+
+x = 0
+
+pencere.mainloop()
--- sunucu.py
+++ sunucu.py
@@ -1,5 +1,131 @@
 from fastapi import FastAPI, Form, Cookie, Header, Response
+from pydantic import BaseModel
+import mysql.connector
+import bcrypt
 
-class serer:
-    server = FastAPI()
-    print("Sunucu açıldı")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+db_user = os.getenv("DB_USER")      
+db_password = os.getenv("DB_PASSWORD")
+
+db_config ={
+'host': 'localhost',
+'user': db_user,
+'password': db_password,
+'database': 'testdb'
+}
+
+app = FastAPI()
+
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+sorgu_SQL = "SELECT Password FROM Kullanıcılar WHERE Nickname = %s"
+
+def sorgu(girilen_kullanici, girilen_sifre):
+        print("sorgu başladı")
+
+        if not girilen_kullanici or not girilen_sifre:
+            print("boş")
+            return False
+
+        cursor.execute(sorgu_SQL, (girilen_kullanici,))
+
+        sonuc = cursor.fetchone()
+
+        if sonuc:
+
+            db_sifre_hash = sonuc[0]
+
+            if isinstance(db_sifre_hash, str):
+                db_sifre_hash = db_sifre_hash.encode('utf-8')
+
+            girilen_sifre_bytes = girilen_sifre.encode('utf-8')
+
+            if bcrypt.checkpw(girilen_sifre_bytes, db_sifre_hash):
+
+                print(f"[BAŞARILI] Giriş yapıldı! Hoş geldiniz: {girilen_kullanici}")
+                cursor.close()
+                conn.close()
+                return True
+                
+                
+            else:
+
+                print("[HATA] Hatalı şifre yada kullanıcı adı!")
+                cursor.close()
+                conn.close()
+                return False
+                
+                
+        else:       
+
+            print("[HATA] Hatalı şifre yada kullanıcı adı!")
+            cursor.close()
+            conn.close()
+            return False
+        
+def kaydet_veritabani(yeni_kullanici, yeni_sifre, yeni_sifre2, email):
+
+    print("Kayıt işlemi başladı...")
+
+    KAYIT_SQL = """
+INSERT INTO Kullanıcılar (Nickname, Password, EMail)
+VALUES (%s, %s, %s)
+"""
+
+    if not yeni_kullanici or not yeni_sifre:
+
+        print("[HATA] Kayıt için kullanıcı adı veya şifre boş bırakılamaz!")
+        return False
+
+    if yeni_sifre != yeni_sifre2:
+        print("şifreyi düzgün tekrarla")
+        return False
+
+    try:
+
+        sifre_bytes = yeni_sifre.encode('utf-8')
+
+        tuz = bcrypt.gensalt()
+
+        hashlenmis_sifre = bcrypt.hashpw(sifre_bytes, tuz)
+
+        cursor.execute(
+            KAYIT_SQL,
+            (yeni_kullanici, hashlenmis_sifre.decode('utf-8'),email)
+        )
+
+        conn.commit()
+
+        print(f"[BAŞARILI] {yeni_kullanici} başarıyla kayıt edildi!")
+        return True
+
+    except mysql.connector.Error as err:
+        if err.errno == 1062:
+            print("[HATA] Bu kullanıcı adı zaten kullanılıyor!")
+        else:
+            print(f"[HATA] Veritabanı hatası: {err}")            
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+class SingupData(BaseModel):
+    username: str
+    password: str
+    password2: str
+    mail: str    
+
+@app.post("/login")
+def login(data: LoginData):
+    if sorgu(data.username, data.password):
+        return {"status": "success"}
+    return {"status": "fail"}
+@app.post("/singup")
+def login(data: SingupData):
+    if kaydet_veritabani(data.username, data.password, data.password2, data.mail):
+        return {"status": "success"}
+    return {"status": "fail"}
